d0f22c5c68b2c9bbebdbceb0cf4c06625db4d16d
tony09uk <tony09uk@gmail.com>
2022-03-28 05:47:30 +0100

    updated var substitution

--- Src/AzureFunctions/RetroArcadeMachines.AzureFunctions.Read.Tests/SendGridEmailQueueTriggerFunctionTests.cs
+++ Src/AzureFunctions/RetroArcadeMachines.AzureFunctions.Read.Tests/SendGridEmailQueueTriggerFunctionTests.cs
@@ -1,17 +1,17 @@
-// namespace RetroArcadeMachines.AzureFunctions.Read.Tests
-// {
-//     using RetroArcadeMachines.AzureFunctions.Read;
-//     using System;
-//     using Xunit;
-//     using Microsoft.Extensions.Logging;
-//     using System.Threading.Tasks;
+namespace RetroArcadeMachines.AzureFunctions.Read.Tests
+{
+    using RetroArcadeMachines.AzureFunctions.Read;
+    using System;
+    using Xunit;
+    using Microsoft.Extensions.Logging;
+    using System.Threading.Tasks;
 
-//     public static class SendGridEmailQueueTriggerFunctionTests
-//     {
-//         [Fact]
-//         public static async Task CannotCallRunWithNullLog()
-//         {
-//             await Assert.ThrowsAsync<ArgumentNullException>(() => SendGridEmailQueueTriggerFunction.Run("TestValue870414907", default(ILogger)));
-//         }
-//     }
-// }
+    public static class SendGridEmailQueueTriggerFunctionTests
+    {
+        [Fact]
+        public static async Task CannotCallRunWithNullLog()
+        {
+            await Assert.ThrowsAsync<ArgumentNullException>(() => SendGridEmailQueueTriggerFunction.Run("TestValue870414907", default(ILogger)));
+        }
+    }
+}
--- Src/AzureFunctions/RetroArcadeMachines.AzureFunctions.Read/SendGridEmailQueueTriggerFunction.cs
+++ Src/AzureFunctions/RetroArcadeMachines.AzureFunctions.Read/SendGridEmailQueueTriggerFunction.cs
@@ -1,38 +1,38 @@
-﻿// using Microsoft.Azure.WebJobs;
-// using Microsoft.Extensions.Logging;
-// using SendGrid;
-// using SendGrid.Helpers.Mail;
-// using System;
-// using System.Threading.Tasks;
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Threading.Tasks;
 
-// namespace RetroArcadeMachines.AzureFunctions.Read
-// {
-//     public static class SendGridEmailQueueTriggerFunction
-//     {
-//         [FunctionName("SendGridEmailQueueTriggerFunction")]
-//         public static async Task Run(
-//             [QueueTrigger("email-queue", Connection = "CloudStorageAccount")] string myQueueItem,
-//             ILogger log)
-//         {
-//             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+namespace RetroArcadeMachines.AzureFunctions.Read
+{
+    public static class SendGridEmailQueueTriggerFunction
+    {
+        [FunctionName("SendGridEmailQueueTriggerFunction")]
+        public static async Task Run(
+            [QueueTrigger("email-queue", Connection = "CloudStorageAccount")] string myQueueItem,
+            ILogger log)
+        {
+            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
 
-//             try
-//             {
-//                 var apiKey = Environment.GetEnvironmentVariable("SendgridAPIKey");
-//                 var client = new SendGridClient(apiKey);
-//                 var from = new EmailAddress("tony09uk@gmail.com", "Retro Arcade Machines");
-//                 var subject = "Query from retro arcade machines";
-//                 var to = new EmailAddress("tony09uk@gmail.com", "Retro Arcade Machines");
-//                 var plainTextContent = "plainTextContent";
-//                 var htmlContent = myQueueItem.ToString();
-//                 var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-//                 var response = await client.SendEmailAsync(msg);
-//             }
-//             catch (Exception ex)
-//             {
-//                 log.LogError($"Error occured while processing QueueItem {myQueueItem} , Exception - {ex.InnerException}");
-//             }
-//         }
-//     }
-// }
+            try
+            {
+                var apiKey = Environment.GetEnvironmentVariable("SendgridAPIKey");
+                var client = new SendGridClient(apiKey);
+                var from = new EmailAddress("tony09uk@gmail.com", "Retro Arcade Machines");
+                var subject = "Query from retro arcade machines";
+                var to = new EmailAddress("tony09uk@gmail.com", "Retro Arcade Machines");
+                var plainTextContent = "plainTextContent";
+                var htmlContent = myQueueItem.ToString();
+                var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+                var response = await client.SendEmailAsync(msg);
+            }
+            catch (Exception ex)
+            {
+                log.LogError($"Error occured while processing QueueItem {myQueueItem} , Exception - {ex.InnerException}");
+            }
+        }
+    }
+}
 
