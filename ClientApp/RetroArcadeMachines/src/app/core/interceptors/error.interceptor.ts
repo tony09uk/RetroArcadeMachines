ad7c462f2a68c2be22124d617e8312a5503c622d
tony09uk <tony09uk@gmail.com>
2022-03-31 20:10:36 +0100

    test error interceptor on live

--- ClientApp/RetroArcadeMachines/src/app/core/interceptors/error.interceptor.ts
+++ ClientApp/RetroArcadeMachines/src/app/core/interceptors/error.interceptor.ts
@@ -16,6 +16,15 @@ export class ErrorInterceptor implements HttpInterceptor {
                 this.authService.signOut();
             }
 
+            if (err.error instanceof Error) {
+                // A client-side or network error occurred. Handle it accordingly.
+                console.error('An error occurred:', err.error.message);
+              } else {
+                // The backend returned an unsuccessful response code.
+                // The response body may contain clues as to what went wrong,
+                console.error(`Backend returned code ${err.status}, body was: ${err.error}`);
+              }
+
             const error = err.error?.message || err.statusText;
             return throwError(error);
         }));
